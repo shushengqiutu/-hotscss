fcd97a4ca96f7e38d24b14cd4ab743eca03fe56d
shushengqiutu <86732994@qq.com>
2019-05-19 21:25:21 +0800

    移动端

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+ @import "../static/reset/reset.css";
+ /* @import "../static/lib/hotcss/px2rem.scss"; */
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>首页</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+ div{
+    width: px2rem(375);
+     background-color: red;
+ }
+
+</style>
+
+
+
